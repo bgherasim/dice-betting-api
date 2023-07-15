132cfe0c911466e31c05b2839b525198e7fff7de
bgherasim <bogdan.gherasim1992@gmail.com>
2023-07-15 21:33:16 +0300

    Use decimal.js for calculations to avoid possible issues caused by JavaScript floating-point representation

--- src/controllers/bet.ts
+++ src/controllers/bet.ts
@@ -1,11 +1,16 @@
+import Decimal from 'decimal.js';
+
 export class BetController {
   static processBet({ betAmount, chance }: {betAmount: number, chance: number}) {
     if (chance < 0 || chance > 100) {
       throw new Error('The chance should be in the [1, 100] interval');
     }
     
-    const payout = Number((betAmount * (100 / chance)).toFixed(2));
-    const rand =  Math.random() * 100;
+    const decimalBetAmount = new Decimal(betAmount);
+    const decimalChance = new Decimal(chance);
+
+    const payout = Number(decimalBetAmount.dividedBy(decimalChance.dividedBy(100)).toFixed(2));
+    const rand = Math.random() * 100;
 
     const win = rand <= chance;
 
